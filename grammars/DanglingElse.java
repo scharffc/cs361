557d4aa76335889ab95564600da132ab87b2720a
Christelle Scharff <scharffc@gmail.com>
2024-10-14 15:33:03 -0400

    Add files via upload

--- grammars/DanglingElse.java
+++ grammars/DanglingElse.java
@@ -1,1 +1,14 @@
+package circle;
 
+public class DanglingElse {
+
+    public static void main(String[] args) {
+        int x = 0;
+        if (x == 5)
+            System.out.println("5");
+        if (x == 6)
+            System.out.println("6");
+        else
+            System.out.println("else");
+    }
+}
